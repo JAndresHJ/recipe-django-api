cef6b45e137efeb182958b476428506dfdefd84a
Jorge Andrés Hernández Jara <39096718+JAndresHJ@users.noreply.github.com>
2020-06-03 13:45:47 -0500

    Adds manage user endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # If it matches the 1st argument it will then get passed to
     # the views which will then render the API
     path('create/', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateTokenView.as_view(), name='token')
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+    path('me/', views.ManageUserView.as_view(), name='me')
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -19,3 +19,15 @@ class CreateTokenView(ObtainAuthToken):
     # The renderer class sets the renderer in order to view
     # this endpoint in the browser, with the browser API
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
